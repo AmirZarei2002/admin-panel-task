f377c479f33c441ee132f6b4ccb23169e76e5849
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-04-22 01:17:14 +0430

    Import the StoreGrowth component and add some styles

--- src/pages/ManagementDashboard.js
+++ src/pages/ManagementDashboard.js
@@ -1,11 +1,15 @@
 import OverallResults from '../components/dashboard-management/OverallResults';
 import Doughnuts from '../components/dashboard-management/Doughnuts';
+import StoreGrowthChart from '../components/dashboard-management/StoreGrowthChart';
 
 export default function ManagementDashboard() {
     return (
         <div>
             <OverallResults />
-            <Doughnuts />
+            <div className="flex flex-col-reverse justify-center lg:justify-between items-center lg:items-start sm:flex-col lg:flex-row sm:w-full px-4 md:px-0 gap-4">
+                <Doughnuts />
+                <StoreGrowthChart />
+            </div>
         </div>
     );
 }
