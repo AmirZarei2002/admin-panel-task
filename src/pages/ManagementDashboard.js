fa536b25e03af8ada959172ee6f6035acb63889b
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-04-22 13:38:03 +0430

    Change width and other styles

--- src/pages/ManagementDashboard.js
+++ src/pages/ManagementDashboard.js
@@ -9,16 +9,26 @@ export default function ManagementDashboard() {
     return (
         <div>
             <OverallResults />
-            <div className="flex flex-col-reverse justify-center lg:justify-between items-center lg:items-start sm:flex-col lg:flex-row sm:w-full px-4 md:px-0 gap-4">
-                <div className="flex flex-col sm:flex-row gap-4">
-                    <TotalExpense />
-                    <TotalIncome />
+            <div className="flex flex-col-reverse justify-center lg:justify-between items-center sm:items-start sm:flex-col md:flex-row px-4 md:px-0 gap-4">
+                <div className="flex flex-col items-center sm:flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-1 xl:space-x-2 w-full lg:w-1/2">
+                    <div className="w-screen sm:w-full lg:w-1/2">
+                        <TotalExpense />
+                    </div>
+                    <div className="w-screen sm:w-full lg:w-1/2">
+                        <TotalIncome />
+                    </div>
                 </div>
-                <StoreGrowthChart />
+                <div className="w-screen sm:w-full md:w-1/2 px-4 md:px-0">
+                    <StoreGrowthChart />
+                </div>
             </div>
-            <div>
-                <ReleasedProduct />
-                <RecentSales />
+            <div className="md:flex">
+                <div className="w-full md:w-1/2 md:p-4 lg:p-3">
+                    <ReleasedProduct />
+                </div>
+                <div className="w-full md:w-1/2 md:p-4 lg:p-3">
+                    <RecentSales />
+                </div>
             </div>
         </div>
     );
