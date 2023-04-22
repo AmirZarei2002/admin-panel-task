4c3fc81306779b6879a77dd3799c450c1ccbc37e
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-04-22 10:25:16 +0430

    Clean up the components

--- src/pages/ManagementDashboard.js
+++ src/pages/ManagementDashboard.js
@@ -1,14 +1,24 @@
 import OverallResults from '../components/dashboard-management/OverallResults';
-import Doughnuts from '../components/dashboard-management/Doughnuts';
 import StoreGrowthChart from '../components/dashboard-management/StoreGrowthChart';
+import ReleasedProduct from '../components/dashboard-management/ReleasedProduct';
+import RecentSales from '../components/dashboard-management/RecentSales';
+import TotalExpense from '../components/dashboard-management/TotalExpense';
+import TotalIncome from '../components/dashboard-management/TotalIncome';
 
 export default function ManagementDashboard() {
     return (
         <div>
             <OverallResults />
             <div className="flex flex-col-reverse justify-center lg:justify-between items-center lg:items-start sm:flex-col lg:flex-row sm:w-full px-4 md:px-0 gap-4">
-                <Doughnuts />
+                <div className="flex flex-col sm:flex-row gap-4">
+                    <TotalExpense />
+                    <TotalIncome />
+                </div>
                 <StoreGrowthChart />
+            </div>
+            <div>
+                <ReleasedProduct />
+                <RecentSales />
             </div>
         </div>
     );
