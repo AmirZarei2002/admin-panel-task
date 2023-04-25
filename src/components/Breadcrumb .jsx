d5eee808abdf892e433dac0c278f6c552451cb13
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-04-25 07:11:48 +0430

    Refactore the functionality

--- src/components/Breadcrumb .jsx
+++ src/components/Breadcrumb .jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 export default function Breadcrumb({ paths, icon: IconComponent, title }) {
     return (
         <div className="flex flex-col-reverse sm:flex-row sm:space-x-2 place-items-end text-[#777] mt-2">
-            <div className="flex space-x-2">
+            <div className="flex space-x-2 items-center">
                 {paths?.reverse().map((path, index) => (
                     <React.Fragment key={index}>
                         {index > 0 && (
-                            <span className="text-md font-mono"> &lt; </span>
+                            <span className="text-md font-mono pt-1"> &lt; </span>
                         )}
-                        <Link to={'/' + path} className="text-lg">
+                        <Link to={'/' + path} className="text-md">
                             {path}
                         </Link>
                     </React.Fragment>
@@ -19,13 +19,14 @@ export default function Breadcrumb({ paths, icon: IconComponent, title }) {
             <div className="flex items-center truncate">
                 <div className="border-r-[1px] border-black/20 pr-2">
                     {IconComponent && (
-                        <IconComponent className="text-xl cursor-pointer" />
+                        <div className="flex items-center space-x-2">
+                            <span className="text-md font-mono pt-1"> &lt; </span>
+                            <IconComponent className="text-xl cursor-pointer" />
+                        </div>
                     )}
                 </div>
                 <div className="pl-2">
-                    {title && (
-                        <h1 className="text-3xl font-semibold">{title}</h1>
-                    )}
+                    {title && <h1 className="text-2xl font-bold">{title}</h1>}
                 </div>
             </div>
         </div>
