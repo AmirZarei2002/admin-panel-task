8549e1175f9cd537e0d1e21b12d9d3e46f851267
AmirZarei2002 <amir.zarei1381913@gmail.com>
2023-04-25 12:57:17 +0430

    Fix the Navbar path

--- src/Layouts/mainLayout.jsx
+++ src/Layouts/mainLayout.jsx
@@ -1,4 +1,4 @@
-import Navbar from '../components/Navbar.jsx';
+import Navbar from '../components/header/Navbar';
 import Breadcrumb from '../components/Breadcrumb ';
 
 export default function MainLayout({ children }) {
