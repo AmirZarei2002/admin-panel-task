323c9d1428016fd76975ec0167b815c25a33fd42
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-04-25 07:43:10 +0430

    Changes in styles

--- src/Layouts/sections/DashTable.jsx
+++ src/Layouts/sections/DashTable.jsx
@@ -4,16 +4,16 @@ export default function DashTable({ mainTitle, headerNames, tableData, rowStyles
     return (
       <div className="relative rounded-xl bg-white">
         <div className="p-3 md:text-end text-start flex flex-row-reverse justify-between">
-          <h2 className="font-semibold">{mainTitle}</h2>
+          <h2 className="font-semibold text-gray-500">{mainTitle}</h2>
           <RxDotsVertical className="cursor-pointer text-xl" />
         </div>
         <div className="border-b-2 mx-3 mb-1" />
-        <div className="overflow-x-auto overflow-y-scrol 2xl:overflow-y-hidden 2xl:overflow-x-hidden max-h-[calc(100vh-6rem)]">
+        <div className="overflow-x-auto overflow-y-scroll 2xl:overflow-y-hidden 2xl:overflow-x-hidden max-h-[calc(120vh-6rem)]">
           <table className="table-auto w-full text-end">
             <thead>
               <tr>
                 {headerNames?.map((headerName, index) => (
-                  <th key={index} className="px-4 py-2 text-sm">
+                  <th key={index} className="px-4 py-2 text-sm truncate">
                     {headerName}
                   </th>
                 ))}
@@ -23,7 +23,7 @@ export default function DashTable({ mainTitle, headerNames, tableData, rowStyles
               {tableData?.map((rowData, rowIndex) => (
                 <tr key={rowIndex} style={rowStyles[rowIndex]} className="border-t">
                   {rowData?.map((cellData, cellIndex) => (
-                    <td key={cellIndex}    className="px-2 py-2 text-gray-600 text-sm font-normal">
+                    <td key={cellIndex} className="px-2 py-2 text-gray-600 text-xs sm:text-sm font-normal">
                       {cellData}
                     </td>
                   ))}
