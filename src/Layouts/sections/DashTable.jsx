6d6df56c02167c48f43486be30a00cac793f7983
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-04-25 07:13:41 +0430

    Implement a new way for passing props

--- src/Layouts/sections/DashTable.jsx
+++ src/Layouts/sections/DashTable.jsx
@@ -1,43 +1,38 @@
 import { RxDotsVertical } from 'react-icons/rx';
 
-export default function DashTable({ data, mainTitle }) {
+export default function DashTable({ mainTitle, headerNames, tableData, rowStyles }) {
     return (
-        <div className="relative rounded-xl bg-white">
-            <div className="p-3 md:text-end text-start flex flex-row-reverse justify-between">
-                <h2 className="font-semibold">{mainTitle}</h2>
-                <RxDotsVertical className="cursor-pointer text-xl" />
-            </div>
-            <div className="border-b-2 mx-3 mb-1" />
-            <div className="overflow-x-auto 2xl:overflow-x-hidden overflow-y-scroll max-h-[calc(100vh-6rem)]">
-                <table className="table-auto w-full text-end">
-                    <thead>
-                        <tr>
-                            {Object.keys(data[0])?.map((header, index) => (
-                                <th
-                                    key={index}
-                                    className="px-4 py-2 text-gray-800 text-md font-bold"
-                                >
-                                    {header}
-                                </th>
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.map((row, index) => (
-                            <tr key={index} className="border-t">
-                                {Object?.values(row).map((cell, index) => (
-                                    <td
-                                        key={index}
-                                        className="px-4 py-2 text-gray-600 even:text-blue-700 even:font-semibold text-sm font-normal"
-                                    >
-                                        {cell}
-                                    </td>
-                                ))}
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+      <div className="relative rounded-xl bg-white">
+        <div className="p-3 md:text-end text-start flex flex-row-reverse justify-between">
+          <h2 className="font-semibold">{mainTitle}</h2>
+          <RxDotsVertical className="cursor-pointer text-xl" />
         </div>
+        <div className="border-b-2 mx-3 mb-1" />
+        <div className="overflow-x-auto 2xl:overflow-x-hidden max-h-[calc(100vh-6rem)]">
+          <table className="table-auto w-full text-end">
+            <thead>
+              <tr>
+                {headerNames?.map((headerName, index) => (
+                  <th key={index} className="px-4 py-2">
+                    {headerName}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {tableData?.map((rowData, rowIndex) => (
+                <tr key={rowIndex} style={rowStyles[rowIndex]} className="border-t">
+                  {rowData?.map((cellData, cellIndex) => (
+                    <td key={cellIndex}    className="px-4 py-2 text-gray-600 text-sm font-normal">
+                      {cellData}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     );
-}
+  }
+  
