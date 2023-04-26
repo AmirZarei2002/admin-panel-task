d536cfb8d3b0c3d4a5bac070551005167e851395
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-04-27 01:30:43 +0430

    Delete and refactored some parts

--- src/Layouts/sections/manage-card/FormDetails.jsx
+++ src/Layouts/sections/manage-card/FormDetails.jsx
@@ -1,22 +1,11 @@
 import { useState } from 'react';
-
-import {
-    Input,
-    InputLabel,
-    Box,
-    Button,
-    Stack,
-    Typography,
-    TextField,
-} from '@mui/material';
+import { Input, InputLabel, Box, Button, Typography } from '@mui/material';
 import FormControl from '@mui/material/FormControl';
 import TextareaAutosize from '@mui/base/TextareaAutosize';
-
-// icons
 import { IoMdHome } from 'react-icons/io';
-import { FaPhoneAlt } from 'react-icons/fa';
+import { FaPhoneAlt, FaSave } from 'react-icons/fa';
 
-export default function FormDetails() {
+export default function FormDetails({ title, buttonTitle }) {
     const [showContact, setShowContact] = useState(true);
 
     const handleContactClick = () => {
@@ -36,14 +25,13 @@ export default function FormDetails() {
                 height: 'max-content',
                 textAlign: 'right',
                 padding: '20px',
-                fullWidth: true,
                 display: 'grid',
+                gap: '20px',
             }}
+            dir="rtl"
         >
-            <Box className="mb-5">
-                <Typography variant="h6">مشخصات شرکت / سازمان</Typography>
-            </Box>
-            <Box className="space-x-10 mb-10 flex flex-row-reverse justify-between">
+            <Typography variant="h6">{title}</Typography>
+            <Box sx={{ display: 'flex', gap: '20px' }}>
                 <FormControl>
                     <InputLabel htmlFor="my-input">شماره ثبت</InputLabel>
                     <Input id="my-input" aria-describedby="my-helper-text" />
@@ -57,137 +45,117 @@ export default function FormDetails() {
                     <Input id="my-input" aria-describedby="my-helper-text" />
                 </FormControl>
             </Box>
-            <Box>
-                <TextField
-                    label="توضیحات"
-                    variant="outlined"
-                    multiline
-                    rows={1}
-                    placeholder="توضیحات"
-                    fullWidth
-                />
-            </Box>
+            <TextareaAutosize
+                sx={{ '&:focus': { border: 'none', outline: 'none' } }}
+                variant="contained"
+                color="primary"
+                placeholder="توضیحات"
+                id="my-input"
+                aria-describedby="my-helper-text"
+            />
             <hr />
             <Box
                 sx={{
                     display: 'flex',
                     justifyContent: 'center',
                     alignItems: 'center',
-                    marginBottom: '10px',
-                    marginTop: '10px',
+                    gap: '20px',
                 }}
             >
                 <Button
                     onClick={handleContactClick}
                     sx={{
                         color: '#626270',
-                        display: 'flex',
-                        alignItems: 'center',
-                        borderBottom: '2px solid black',
+                        borderBottom: showContact ? '2px solid black' : 'none',
                         paddingBottom: '5px',
                     }}
                 >
-                    <Typography className="pr-3">اطلاعات تماس</Typography>
-                    <FaPhoneAlt className="text-md sm:text-md lg:text-xl" />
+                    <Typography className="pr-3 pl-1">اطلاعات تماس</Typography>
+                    <FaPhoneAlt className="text-lg" />
                 </Button>
                 <Button
                     onClick={handleAddressClick}
                     sx={{
                         color: '#626270',
-                        textWeight: '10rem',
-                        borderBottom: '2px solid black',
+                        borderBottom: !showContact ? '2px solid black' : 'none',
                         paddingBottom: '5px',
-                        marginLeft: '20px',
                     }}
                 >
-                    <Typography className="pr-3">اطلاعات آدرس</Typography>
-                    <IoMdHome className="text-md sm:text-md lg:text-xl" />
+                    <Typography className="pr-3 pl-1">اطلاعات آدرس</Typography>
+                    <IoMdHome className="text-xl" />
                 </Button>
             </Box>
             <hr />
-            {!showContact ? (
-                <Box className="items-center flex flex-col space-y-10 mt-[25px]">
-                    <Box className="space-x-20">
-                        {' '}
-                        <FormControl>
-                            <InputLabel htmlFor="my-input">
-                                عنوان شرکت
-                            </InputLabel>
-                            <Input
-                                id="my-input"
-                                aria-describedby="my-helper-text"
-                            />
-                        </FormControl>
-                        <FormControl>
-                            <InputLabel htmlFor="my-input">
-                                عنوان شرکت
-                            </InputLabel>
-                            <Input
-                                id="my-input"
-                                aria-describedby="my-helper-text"
-                            />
-                        </FormControl>
-                        <FormControl>
-                            <InputLabel htmlFor="my-input">
-                                عنوان شرکت
-                            </InputLabel>
-                            <TextareaAutosize
-                                maxRows
-                                id="my-input"
-                                aria-describedby="my-helper-text"
-                            />
-                        </FormControl>
-                    </Box>
-                    <Box className="space-x-10" style={{ marginBottom: '50x' }}>
-                        <FormControl>
-                            <InputLabel htmlFor="my-input">
-                                عنوان شرکت
-                            </InputLabel>
-                            <Input
-                                id="my-input"
-                                aria-describedby="my-helper-text"
-                            />
-                        </FormControl>
-                        <FormControl>
-                            <InputLabel htmlFor="my-input">
-                                عنوان شرکت
-                            </InputLabel>
-                            <Input
-                                id="my-input"
-                                aria-describedby="my-helper-text"
-                            />
-                        </FormControl>
-                    </Box>
+            {showContact ? (
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: '20px',
+                        marginTop: '25px',
+                    }}
+                >
+                    <FormControl>
+                        <InputLabel htmlFor="my-input">نام شرکت</InputLabel>
+                        <Input
+                            id="my-input"
+                            aria-describedby="my-helper-text"
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <InputLabel htmlFor="my-input">شماره تلفن</InputLabel>
+                        <Input
+                            id="my-input"
+                            aria-describedby="my-helper-text"
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <InputLabel htmlFor="my-input">ایمیل</InputLabel>
+                        <Input
+                            id="my-input"
+                            aria-describedby="my-helper-text"
+                        />
+                    </FormControl>
                 </Box>
             ) : (
-                <Box className="items-center flex flex-col space-y-10 mt-[25px]">
-                    <Box className="space-x-20">
-                        <Box
-                            className="space-x-10"
-                            style={{ marginBottom: '50x' }}
-                        >
-                            <FormControl>
-                                <InputLabel htmlFor="my-input">
-                                    عنوان شرکت
-                                </InputLabel>
-                                <Input
-                                    id="my-input"
-                                    aria-describedby="my-helper-text"
-                                />
-                            </FormControl>
-                            <FormControl>
-                                <InputLabel htmlFor="my-input">
-                                    عنوان شرکت
-                                </InputLabel>
-                                <Input
-                                    id="my-input"
-                                    aria-describedby="my-helper-text"
-                                />
-                            </FormControl>
-                        </Box>
-                    </Box>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: '20px',
+                        marginTop: '25px',
+                    }}
+                >
+                    <FormControl>
+                        <InputLabel htmlFor="my-input">آدرس</InputLabel>
+                        <Input
+                            id="my-input"
+                            aria-describedby="my-helper-text"
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <InputLabel htmlFor="my-input">کدپستی</InputLabel>
+                        <Input
+                            id="my-input"
+                            aria-describedby="my-helper-text"
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <InputLabel htmlFor="my-input">شهر</InputLabel>
+                        <Input
+                            id="my-input"
+                            aria-describedby="my-helper-text"
+                        />
+                    </FormControl>
                 </Box>
             )}
+            <Button
+                variant="contained"
+                className="font-semibold text-white bg-slate-700 text-start px-4 py-2 rounded-md flex items-center gap-3 w-fit"
+                startIcon={<FaSave className="text-lg" />}
+            >
+                <Box sx={{ fontSize: '0.875rem' }}>{buttonTitle}</Box>
+            </Button>
         </Box>
     );
 }
