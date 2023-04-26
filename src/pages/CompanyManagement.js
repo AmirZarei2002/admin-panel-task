0ffd307f9a09c5dc028ad604afa0cf4e814e2092
AmirZarei2002 <amir.zarei1381913@gmail.com>
2023-04-26 17:27:21 +0430

    Add Routes

--- src/pages/CompanyManagement.js
+++ src/pages/CompanyManagement.js
@@ -1,9 +1,13 @@
-import ManageCard from "../Layouts/sections/ManageCard";
+import BoxDetails from '../Layouts/sections/manage-card/BoxDetails';
+import FormDetails from '../Layouts/sections/manage-card/FormDetails';
 
 export default function CompanyManagement() {
-  return (
-    <div>
-        <ManageCard />
-    </div>
-  )
+    return (
+        <main className="w-full flex flex-col-reverse lg:flex-row justify-center px-4 md:px-0 gap-5 2xl:space-x-2 lg:px-6">
+            {/* Left Card */}
+            <FormDetails />
+            {/* Right Card */}
+            <BoxDetails />
+        </main>
+    );
 }
