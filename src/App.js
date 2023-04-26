c2566827b4cd654dbcaaddddf18aab0e5980eb4e
Amirhossein Zarei <103286828+AmirZarei2002@users.noreply.github.com>
2023-04-27 01:28:08 +0430

    Change the path of company management

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import MainLayout from './Layouts/mainLayout';
 import ManagementDashboard from './pages/ManagementDashboard';
-import CompanyManagement from './pages/CompanyManagement';
 import UsersList from './pages/users-management/UsersList';
+import NewCompany from './pages/company-management/NewCompany';
 
 const App = () => {
     return (
@@ -12,7 +12,7 @@ const App = () => {
                 <Routes>
                     {/* Admin Management Dashboard */}
                     <Route path="/" element={<ManagementDashboard />} />
-                    <Route path="/create/company" element={<CompanyManagement />} />
+                    <Route path="/create/company" element={<NewCompany />} />
                     <Route path="/user-list" element={<UsersList />} />
 
                 </Routes>
